0aaf780ccedf9752cb85ccc4649236d0c0f86106
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2020-09-29 20:34:35 +0200

    First build

---
+++ .next/static/n0ta5Q6oqtYvEyur2fAi0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fbd2d405067cdf50a4596.css","static\u002Fchunks\u002Fpages\u002Findex-f86b7cd8fc5a2aedc5f4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
